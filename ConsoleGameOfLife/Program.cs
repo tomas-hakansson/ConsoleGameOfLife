776abf8f7723364c6fe066e5adaab366cc31cf3e
Tomas Håkansson <tomas.hakansson@gmail.com>
2018-04-16 21:08:01 +0200

    I completed the step functionality.

--- ConsoleGameOfLife/Program.cs
+++ ConsoleGameOfLife/Program.cs
@@ -1,63 +1,113 @@
-﻿using System;
-using GameOfLife;
+﻿using GameOfLife;
+using System;
 using System.Threading;
 
 namespace ConsoleGameOfLife
 {
     class Program
     {
+        static bool Quit = false;
+        static bool Pause = false;
+        static bool Step = false;
+
         static void Main(string[] args)
         {
             try
             {
                 World world = null;
-                var pa = new ArgsParser(args);
+                var parser = new ArgsParser(args);
 
-                switch (pa.SourceType)
+                switch (parser.SourceType)
                 {
                     case InitialWorld.Random:
-                        var ranWorld = HelperMethods.GenerateRandomWorld(pa.Width, pa.Height);
-                        world = new World(ranWorld, pa.FixedSize);
+                        var ranWorld = HelperMethods.GenerateRandomWorld(parser.Width, parser.Height);
+                        world = new World(ranWorld, parser.FixedSize);
                         break;
                     case InitialWorld.Sample:
-                        var sample = Samples.Get(pa.Source);
-                        world = new World(sample, pa.FixedSize);
+                        var sample = Samples.Get(parser.Source);
+                        world = new World(sample, parser.FixedSize);
                         break;
                     case InitialWorld.Raw:
-                        world = new World(pa.Source, pa.FixedSize);
-                        break;
-                    default:
+                        world = new World(parser.Source, parser.FixedSize);
                         break;
                 }
-                do
+
+                Thread ConsoleKeyListener = new Thread(new ThreadStart(KeyboardEvents));
+                ConsoleKeyListener.Start();
+
+                var wasGrowing = false;
+                while (!Quit)
                 {
-                    while (!Console.KeyAvailable)
+                    if (Pause)
                     {
-                        var currentWorld = world.CurrentWorld;
-                        var width = currentWorld[0].Count;
-                        var height = currentWorld.Count;
-                        if (pa.SourceType != InitialWorld.Random && pa.FixedSize && (width < pa.Width || height < pa.Height))
-                            world = new World(world.CurrentWorld, false);
-                        else if (pa.SourceType != InitialWorld.Random && pa.FixedSize)
-                            world = new World(world.CurrentWorld, pa.FixedSize);
-                        //if (Console.ReadKey(true).Key == ConsoleKey.Q)
-                        //    break;
-                        Console.Clear();
-                        Console.WriteLine("Press 'q' to exit!");
-                        Console.WriteLine();
-                        var toPrint = HelperMethods.MatrixToString(world.CurrentWorld);
-                        Console.WriteLine(toPrint);
-                        world = world.NextGeneration();
-                        Thread.Sleep(150);
+                        if (Step)
+                        {
+                            RunGameOfLife("Press 'q' to quit, 's' to step through each generation, and 'r' to resume!", ref world, parser, ref wasGrowing);
+                            Step = false;
+                        }
+                    }
+                    else
+                    {
+                        RunGameOfLife("Press 'q' to quit and 'p' to pause!", ref world, parser, ref wasGrowing);
                     }
                 }
-                while (Console.ReadKey(true).Key != ConsoleKey.Q);
+
+                Environment.Exit(0);
             }
             catch (Exception ex)
             {
                 Console.Write(ex.Message);
                 Console.ReadKey();
             }
+        }
+
+        private static void RunGameOfLife(string text, ref World world, ArgsParser pa, ref bool wasGrowing)
+        {
+            var currentWorld = world.CurrentWorld;
+            var width = currentWorld[0].Count;
+            var height = currentWorld.Count;
+            if (pa.SourceType != InitialWorld.Random && pa.FixedSize && (width < pa.Width || height < pa.Height))
+            {
+                world = new World(world.CurrentWorld, false);
+                wasGrowing = true;
+            }
+            else if (wasGrowing)
+                world = new World(world.CurrentWorld, pa.FixedSize);
+
+            Console.Clear();
+            Console.WriteLine(text);
+            Console.WriteLine();
+            var toPrint = HelperMethods.MatrixToString(world.CurrentWorld);
+            Console.WriteLine(toPrint);
+            world = world.NextGeneration();
+            Thread.Sleep(150);
+        }
+
+        private static void KeyboardEvents()
+        {
+            do
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.Q:
+                        Quit = true;
+                        break;
+                    case ConsoleKey.P:
+                        Pause = true;
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine("Press 'q' to quit, 's' to step through each generation, and 'r' to resume!");
+                        break;
+                    case ConsoleKey.S://step
+                        Step = true;
+                        break;
+                    case ConsoleKey.R://Resume
+                        Pause = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (true);
         }
     }
 }
